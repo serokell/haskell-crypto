edaaefc15805f83d186bcdd41bf74384c4d6029f
Kirill Elagin <kirelagin@gmail.com>
2021-07-17 16:53:59 -0400

    meta: Update the flake and enable checks
    When this project was originally started, flakes were not in active use yet.
    
    * Remove the obsolete `edition` attribute.
    * Add an actual flake.lock file.
    * Bump the haskell.nix version.
    * Run Haskell tests as flake checks.
    * Add a reuse lint check.
    
    There are two ugly workarounds going on here, both related to the use of
    `../hpack/common.yaml` in `package.yaml` files:
    
    1. I had to rename `common.yaml` to `project.yaml`, because there is
       some weird logic early in the stack-to-nix conversion process that
       cleans up the source, leaving only `stack.yaml`, `*.cabal`, and
       `project.yaml` and then, apparently, it tried to parse the
       `project.yaml`, including all its imports, and fails, since it has
       cleaned `../hpack/common.yaml` away.
       That’s some new logic, I have new idea what it does and why, and I
       did not find any way to disable it, hence the weird rename.
    2. Further in the process, there is a different cleaning step that,
       again, removed `../hpack/common.yaml`, and this time even renaming it
       does not help. That should be perfectly fine, since we already have
       `.cabal` files committed in the repo, but, for some reason, it still
       decides to run `hpack`, which inevitably fails, since the file has
       been cleaned away.
       This is addressed by another workaround (which has already been
       there) that just forcefully disabled the call to hpack.
    
    Both of these are clearly bugs in haskell.nix – it should not care about
    `project.yaml` if there is a `.cabal` file in the source directory.

--- flake.nix
+++ flake.nix
@@ -3,15 +3,47 @@
 # SPDX-License-Identifier: MPL-2.0
 
 {
-  edition = 201911;
-
   description = "Easy-and-safe-to-use high-level cryptographic primitives.";
 
-  outputs = { self, nixpkgs, haskell-nix }:
+  inputs = {
+    nixpkgs.url = "github:serokell/nixpkgs";
+
+    hackage = {
+      url = "github:input-output-hk/hackage.nix";
+      flake = false;
+    };
+    stackage = {
+      url = "github:input-output-hk/stackage.nix";
+      flake = false;
+    };
+    haskell-nix = {
+      url = "github:input-output-hk/haskell.nix/81fb54dbfdd350b6ad8271973545f1b668975394";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+
+    flake-utils.url = "github:numtide/flake-utils";
+
+    flake-compat = {
+      url = "github:edolstra/flake-compat";
+      flake = false;
+    };
+  };
+
+  outputs = { self, nixpkgs, hackage, stackage, haskell-nix, flake-utils, flake-compat }:
+  flake-utils.lib.eachSystem [ "x86_64-linux" ] (system:
     let
-      pkgs = nixpkgs {
-        inherit (haskell-nix) config overlays;
+      pkgs = import nixpkgs {
+        inherit system;
+        overlays = [
+          (haskell-nix.internal.overlaysOverrideable {
+            sourcesOverride = haskell-nix.internal.sources // {
+              inherit hackage stackage;
+            };
+          }).combined-eval-on-build
+        ];
       };
+
+      inherit (nixpkgs.lib) mapAttrs mapAttrs';
 
       project = pkgs.haskell-nix.stackProject {
         src = pkgs.haskell-nix.haskellLib.cleanGit {
@@ -25,22 +57,29 @@
               libsodium.components.library.build-tools = [ project.c2hs ];
 
               # TODO: https://github.com/input-output-hk/haskell.nix/issues/626
+              # (also, it gets cleaned away for some reason)
               NaCl.cabal-generator = pkgs.lib.mkForce null;
               crypto-sodium.cabal-generator = pkgs.lib.mkForce null;
+              # TODO: rename ./hpack/package.yaml back to ./hpack/common.yaml
+              # (the name had to be changed as otherwise it gets cleaned in the process)
             };
           })
         ];
       };
-      inherit (project) NaCl crypto-sodium;
-    in {
-      packages = {
-        NaCl = NaCl.components.library;
-        crypto-sodium = crypto-sodium.components.library;
+
+      local = {
+        inherit (project) NaCl crypto-sodium;
       };
+    in rec {
+      packages = mapAttrs (_: p: p.components.library) local;
 
       checks = {
-        build = self.packages.NaCl;
-        test = NaCl.checks.test;
-      };
-    };
+        reuse = pkgs.runCommand "reuse-lint" {
+          nativeBuildInputs = [ pkgs.reuse ];
+        } ''reuse --root ${./.} lint > "$out"'';
+      } //
+      (mapAttrs' (n: p: { name = "build-" + n; value = p; }) packages) //
+      (mapAttrs' (n: p: { name = "test-" + n; value = p.checks.test; }) local);
+    }
+  );
 }
